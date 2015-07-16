21ff954c1c3e4730447659149d94d4371dcce9fe
Benoit VALLON <benoitvallon@gmail.com>
2015-07-16 13:59:48 -0400

    Fixes tab

--- web-servers.go
+++ web-servers.go
@@ -1,23 +1,23 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"net/http"
+  "fmt"
+  "log"
+  "net/http"
 )
 
 type Hello struct{}
 
 func (h Hello) ServeHTTP(
-	w http.ResponseWriter,
-	r *http.Request) {
-	fmt.Fprint(w, "Hello!")
+  w http.ResponseWriter,
+  r *http.Request) {
+  fmt.Fprint(w, "Hello!")
 }
 
 func main() {
-	var h Hello
-	err := http.ListenAndServe("localhost:4000", h)
-	if err != nil {
-		log.Fatal(err)
-	}
+  var h Hello
+  err := http.ListenAndServe("localhost:4000", h)
+  if err != nil {
+    log.Fatal(err)
+  }
 }
