beff5cf5c1bcc1a306d92d3ca8525e3c5b3ab129
Benoit VALLON <benoitvallon@gmail.com>
2015-07-16 12:00:23 -0400

    Fixes tab formatting

--- exercise-stringer.go
+++ exercise-stringer.go
@@ -6,15 +6,15 @@ type IPAddr [4]byte
 
 // TODO: Add a "String() string" method to IPAddr.
 func (p IPAddr) String() string {
-	return fmt.Sprintf("\"%d.%d.%d.%d\"", p[0], p[1], p[2], p[3])
+  return fmt.Sprintf("\"%d.%d.%d.%d\"", p[0], p[1], p[2], p[3])
 }
 
 func main() {
-	addrs := map[string]IPAddr{
-		"loopback":  {127, 0, 0, 1},
-		"googleDNS": {8, 8, 8, 8},
-	}
-	for n, a := range addrs {
-		fmt.Printf("%v: %v\n", n, a)
-	}
+  addrs := map[string]IPAddr{
+    "loopback":  {127, 0, 0, 1},
+    "googleDNS": {8, 8, 8, 8},
+  }
+  for n, a := range addrs {
+    fmt.Printf("%v: %v\n", n, a)
+  }
 }
